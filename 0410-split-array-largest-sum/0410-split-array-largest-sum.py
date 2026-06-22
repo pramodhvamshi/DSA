b75a21c476efc9d01a35c17fc327d2ce228aa0ee
Pramodh Vamshi <pramodhvamshi@gmail.com>
2026-06-22 22:29:35 +0530

    Time: 2 ms (77.13%), Space: 19.3 MB (82.33%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def func(self, nums: List[int],maxSum:int) -> int:
+        n=len(nums)
+        partitions=1
+        subarraySum=0
+        for i in range(n):
+            if subarraySum + nums[i]<=maxSum:
+                subarraySum+=nums[i]
+            else:
+                partitions+=1
+                subarraySum=nums[i]
+        return partitions
+    def splitArray(self, nums: List[int], k: int) -> int:
+        low=max(nums)
+        high=sum(nums)
+        while low<=high:
+            mid=(low+high)//2
+            partitions=self.func(nums,mid)
+            if partitions>k:
+                low=mid+1
+            else:
+                high=mid-1
+        return low
+        
