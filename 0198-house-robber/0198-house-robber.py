c3e7acd2cb06a4a26b6dd5f5cd170655bffbbb10
Pramodh Vamshi <pramodhvamshi@gmail.com>
2026-07-19 22:47:24 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.11%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,14 @@
+#Space Optimization
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        prev=nums[0]
+        prev2=0
+        for i in range(1,n):
+            pick=nums[i] 
+            if (i>1): pick+=prev2
+            notPick=0+prev
+            curi=max(pick,notPick)
+            prev2=prev
+            prev=curi
+        return prev
